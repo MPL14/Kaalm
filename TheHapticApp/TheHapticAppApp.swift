77c503a98987f2ad6d5c7dad2c7cd4eb069ac314
achi113s <latourgiorgio@gmail.com>
2024-01-25 01:14:05 -0500

    first commit

---
+++ TheHapticApp/TheHapticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHapticAppApp.swift
+//  TheHapticApp
+//
+//  Created by Giorgio Latour on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TheHapticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
