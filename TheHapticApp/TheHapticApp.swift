70b9cc3bc3f1ede4b176196501f4878b23700375
achi113s <latourgiorgio@gmail.com>
2024-02-21 17:53:28 -0500

    minor fixes

--- TheHapticApp/TheHapticApp.swift
+++ TheHapticApp/TheHapticApp.swift
@@ -13,7 +13,7 @@ struct TheHapticApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     @StateObject private var hapticEffects: HapticEngine = HapticEngine()
-    
+
     @AppStorage(Constants.onboardingComplete) var onboardingComplete: Bool = false
     @AppStorage(Constants.darkModePreferred) var darkModePreferred: Bool = false
 
@@ -38,11 +38,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        #if DEBUG
-        Purchases.logLevel = .error
-        #endif
-        Purchases.configure(withAPIKey: rcPublicApiKey)
+#if DEBUG
+            Purchases.logLevel = .debug
+#endif
+            Purchases.configure(withAPIKey: rcPublicApiKey)
 
-        return true
-    }
+            return true
+        }
 }
